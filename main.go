319d318ce54d07ed92edd1a4446571aa85b743ef
doonyango <datieno001@gmail.com>
2024-09-01 03:35:49 +0300

    refactor: update the module for efficiency

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	"guessit/calculate"
+)
+
+func main() {
+	var numbers []float64
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		number, err := strconv.ParseFloat(line, 64)
+		if err != nil {
+			fmt.Println("Invalid input:", err)
+			continue
+		}
+		numbers = append(numbers, number)
+
+		lowerBound, upperBound := calculate.CalculateRange(numbers)
+		fmt.Printf("%.0f %.0f\n", lowerBound, upperBound)
+	}
+}
