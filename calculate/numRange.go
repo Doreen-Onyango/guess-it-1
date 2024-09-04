c3304f42773ab6d80795031d2c3ba1f2c7e1f02a
doonyango <datieno001@gmail.com>
2024-09-05 02:47:05 +0300

    refactor: remove unused code

--- calculate/numRange.go
+++ calculate/numRange.go
@@ -5,21 +5,12 @@ func CalculateRange(num []float64) (float64, float64) {
 		return 0, 1000
 	}
 
-	// Calculate the difference between each consecutive pair of numbers.
-	dif := make([]float64, len(num)-1)
-	for i := 1; i < len(num); i++ {
-		dif[i-1] = num[i] - num[i-1]
-	}
-
-	meanDiff := Mean(dif)
-	stdDiff := StandardDeviation(dif)
-
-	lastNum := num[len(num)-1]
-	predict := lastNum + meanDiff
+	meanDiff := Mean(num)
+	stdDiff := StandardDeviation(num)
 
 	// Capture variability around the predicted value.
-	minLimit := predict - 2*stdDiff
-	maxLimit := predict + 2*stdDiff
+	minLimit := meanDiff - 2*stdDiff
+	maxLimit := meanDiff + 2*stdDiff
 
 	return minLimit, maxLimit
 }
