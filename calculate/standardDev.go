db63062d3fff4e68aabc8ebbdf3122d6a64669b8
doonyango <datieno001@gmail.com>
2024-09-05 02:49:47 +0300

    fix: correct variance to work well

--- calculate/standardDev.go
+++ calculate/standardDev.go
@@ -11,12 +11,14 @@ func Variance(data []float64) float64 {
 		fmt.Println("Provide Data")
 		os.Exit(0)
 	}
-	var diff float64
+	mean := Mean(data)
+	out := []float64{}
+
 	for _, num := range data {
-		d := num - Mean(data)
-		diff += d * d
+		dev := num - mean
+		out = append(out, dev*dev)
 	}
-	return diff / float64(len(data))
+	return Mean(out)
 }
 
 func StandardDeviation(data []float64) float64 {
