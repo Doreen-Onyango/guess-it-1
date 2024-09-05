5b72c3c7ed80495869d7683d06b22d418cb85ac4
doonyango <datieno001@gmail.com>
2024-09-05 16:19:02 +0300

    test: modify test

--- calculate/numRange_test.go
+++ calculate/numRange_test.go
@@ -8,14 +8,15 @@ import (
 
 func TestCalculateRange(t *testing.T) {
 	input := []float64{100, 200, 300, 400, 500}
-	expected := 600.0
+	expected1 := 17.0
+	expected2 := 583.0
 	res1, res2 := CalculateRange(input)
 	roundres1 := math.Round(res1)
 	roundres2 := math.Round(res2)
-	if !reflect.DeepEqual(roundres1, expected) {
-		t.Errorf("test failed. expected : %v, got : %v", expected, roundres1)
+	if !reflect.DeepEqual(roundres1, expected1) {
+		t.Errorf("test failed. expected : %v got : %v", expected1, roundres1)
 	}
-	if !reflect.DeepEqual(roundres2, expected) {
-		t.Errorf("test failed. expected : %v, got : %v", expected, roundres2)
+	if !reflect.DeepEqual(roundres2, expected2) {
+		t.Errorf("test failed. expected : %v got : %v", expected2, roundres2)
 	}
 }
