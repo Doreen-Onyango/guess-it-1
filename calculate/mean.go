071fde5b5829ae18bfb5abada49346faf90e075b
doonyango <datieno001@gmail.com>
2024-09-13 17:37:54 +0300

    docs: add comment

--- calculate/mean.go
+++ calculate/mean.go
@@ -11,8 +11,17 @@ func Mean(data []float64) float64 {
 		fmt.Println("Provide Data")
 		os.Exit(0)
 	}
+	n := len(data)
+	if n > 5 {
+		n = 5
+	}
+
+	// Get the last n elements
+	start := len(data) - n
+	recentData := data[start:]
+
 	var sum float64
-	for _, value := range data {
+	for _, value := range recentData {
 		sum += value
 	}
 	return sum / float64(len(data))
