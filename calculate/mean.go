7e626779714c139aa98dbe4bfe7a5f7b6095bad3
doonyango <datieno001@gmail.com>
2024-09-14 12:38:47 +0300

    refactor: remove unused code

--- calculate/mean.go
+++ calculate/mean.go
@@ -11,17 +11,8 @@ func Mean(data []float64) float64 {
 		fmt.Println("Provide Data")
 		os.Exit(0)
 	}
-	n := len(data)
-	if n > 5 {
-		n = 5
-	}
-
-	// Get the last n elements
-	start := len(data) - n
-	recentData := data[start:]
-
 	var sum float64
-	for _, value := range recentData {
+	for _, value := range data {
 		sum += value
 	}
 	return sum / float64(len(data))
